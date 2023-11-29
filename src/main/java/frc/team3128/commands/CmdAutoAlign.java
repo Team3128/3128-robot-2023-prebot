977aca1dccff2bceade52635330769ea170d5b55
Sophia <113397611+SophiaTao13@users.noreply.github.com>
2023-11-28 19:00:59 -0800

    commit3
    started on cmdautoalign, currently getting pythonll to work

--- src/main/java/frc/team3128/commands/CmdAutoAlign.java
+++ src/main/java/frc/team3128/commands/CmdAutoAlign.java
@@ -1,3 +1,19 @@
-public class CmdAutoAlign {
+package frc.team3128.commands;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.team3128.subsystems.LimelightSubsystem;
+import frc.team3128.subsystems.Swerve;
+
+public class CmdAutoAlign extends CommandBase {
+    private LimelightSubsystem m_limelight;
+    private Swerve m_swerve;
+
+    public CmdAutoAlign() {
+        m_limelight = LimelightSubsystem.getInstance();
+        m_swerve = Swerve.getInstance();
+
+        //get states done 
+
+    }
+
     
 }
